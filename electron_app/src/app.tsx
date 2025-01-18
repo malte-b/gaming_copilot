88b0be09018964ece790109e2b915ca65146900c
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2025-01-18 13:01:29 +0100

    feat(init): setup electron_app with react and typescript

---
+++ electron_app/src/app.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import Wrapper from "./main-process/wrapper";
+
+const root = createRoot(document.body);
+root.render(<Wrapper />);
---
+++ electron_app/src/main-process/wrapper.tsx
@@ -0,0 +1,3 @@
+export default function Wrapper() {
+  return <div>Hello there!</div>;
+}

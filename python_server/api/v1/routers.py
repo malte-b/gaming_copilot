a3fa1f4cbcb2d167712aa689d6ba4b7410712c5f
malte-b <malte.barth01@gmail.com>
2025-01-19 12:03:01 +0100

    add route

--- python_server/api/v1/routers.py
+++ python_server/api/v1/routers.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
-from api.v1.endpoints import generate_langchain_response_endpoint
+from api.v1.endpoints import generate_langchain_response_endpoint, vision_screenshot_endpoint
 
 api_router = APIRouter()
 
 api_router.include_router(generate_langchain_response_endpoint.router, prefix="", tags=["generate_langchain_response_endpoint"])
+api_router.include_router(vision_screenshot_endpoint.router, prefix="", tags=["vision_screenshot_endpoint"])
+
